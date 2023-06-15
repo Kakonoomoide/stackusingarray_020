9e6e7bcc4a6a021e299e3e1a6b1caaa1eb542c84
Kakonoomoide <rizkiramada128@gmail.com>
2023-06-15 11:19:37 +0700

    membuat methode pop (hapus)

--- stackusingarray_020.cpp
+++ stackusingarray_020.cpp
@@ -28,6 +28,16 @@ public:
         return element;
     }
 
+    void pop() {
+        if (empty()) {
+            cout << "\nStack is empty. cannot pop." << endl;
+            return;
+        }
+
+        cout << "\nThe poped element is: " << stack_array[top] << endl;
+        top--;
+    }
+
 
 };
 
